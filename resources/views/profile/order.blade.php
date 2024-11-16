b3c363d2dc662f508dc358ae526f449d6a27030d
Mehdi <nikanuser@gmail.com>
2024-11-16 22:45:52 +0330

    complating list order

--- resources/views/profile/order.blade.php
+++ resources/views/profile/order.blade.php
@@ -16,8 +16,9 @@
                 <div class="card-body p-4">
                     <!-- Single Order Status-->
                     @foreach ($orders as $order)
-                        <div class="single-order-status cursor-pointer {{ $order->getRawOriginal('status') ? 'active' :'' }}" data-bs-toggle="collapse" data-bs-target="#details1"
-                            aria-expanded="false" aria-controls="details1">
+                        <div class="single-order-status cursor-pointer {{ $order->getRawOriginal('status') ? 'active' : '' }}"
+                            data-bs-toggle="collapse" data-bs-target="#details{{ $order->id }}" aria-expanded="false"
+                            aria-controls="details{{ $order->id }}">
                             <div class="order-icon">
                                 <i class="ti ti-basket"></i>
                             </div>
@@ -31,17 +32,17 @@
 
                         </div>
                         <!-- Dropdown Content -->
-                        <div class="collapse" id="details1">
+                        <div class="collapse" id="details{{ $order->id }}">
                             <div class="order-details">
                                 <div class="card-body ">
                                     <ul class="list-group">
-                                        <li class="list-group-item d-flex justify-content-between">
-                                            <span class="text-end">مبلغ تخفیف</span>
-                                            <span class="text-start">{{ number_format($coupnPrice) }} تومان</span>
-                                        </li>
-                                        
+                                        @foreach ($order->orderItems as $item)
+                                            <li class="list-group-item d-flex justify-content-between">
+                                                <span class="text-start">{{ number_format($item->subtotal) }} تومان</span>
+                                                <span class="text-end">{{ $item->product->name }} * {{ $item->quantity }}</span>
+                                            </li>
+                                        @endforeach
                                     </ul>
-                                   
                                 </div>
                             </div>
                         </div>
